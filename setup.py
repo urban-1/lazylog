b0c0c6c67cc511b0f212a1932bb7dae10b8e65bb
Andreas Bontozoglou <urban@fb.com>
2020-03-14 10:58:41 +0000

    [setup] Preparing release

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(
     'Topic :: System :: Logging',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 2.6',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
